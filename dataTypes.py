cb3d2a56c2a5774ee34802ffe635ddea60bb82b7
Efe Can Kara <efecan_kara@proton.me>
2023-01-10 14:47:43 +0300

    Data Types cheatsheet completed.

---
+++ dataTypes.py
@@ -0,0 +1,61 @@
+print("You can get the data type of any object by using the type() function")
+
+x = 5
+print(type(x))
+
+x = "Hello World"
+print(type(x))
+
+x = 20.5
+print(type(x))
+
+x = 1j
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+x = range(6)
+print(type(x))
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+x = {"apple", "banana", "cherry"}
+print(type(x))
+
+x = frozenset({"apple", "banana", "cherry"})
+print(type(x))
+
+x = True
+print(type(x))
+
+x = b"Hello"
+print(type(x))
+
+x = bytearray(5)
+print(type(x))
+
+x = memoryview(bytes(5))
+print(type(x))
+
+print("For specify the data type, you can use the following constructor functions.")
+
+x = str("Hello World")
+x = int(20)
+x = float(20.5)
+x = complex(1j)
+x = list(("apple", "banana", "cherry"))
+x = tuple(("apple", "banana", "cherry"))
+x = range(6)
+x = dict(name="John", age=36)
+x = set(("apple", "banana", "cherry"))
+x = frozenset(("apple", "banana", "cherry"))
+x = bool(5)
+x = bytes(5)
+x = bytearray(5)
+x = memoryview(bytes(5))
+
